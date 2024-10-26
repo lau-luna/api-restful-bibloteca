72fc46f1abf07d56c12391e4b03f58bfd4eb4dbd
lautaro-luna <128639299+lau-luna@users.noreply.github.com>
2024-10-26 11:56:22 -0300

    Algo del deploy

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -9,41 +9,39 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id(); // Columna auto_increment y clave primaria
             $table->string('name', 50);
             $table->string('surname', 100);
             $table->string('email', 255)->unique();
-            $table->unsignedInteger('telephone', 20);
+            $table->unsignedBigInteger('telephone'); // Usar unsignedBigInteger para un número de teléfono
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password', 255);
             $table->string('role', 20);
             $table->string('image')->nullable();
             $table->rememberToken();
             $table->timestamps();
-
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            $table->string('email')->primary(); // Columna email como clave primaria
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
 
         Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
+            $table->string('id')->primary(); // Columna id como clave primaria
+            $table->foreignId('user_id')->nullable()->index(); // Clave foránea a la tabla users
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
             $table->integer('last_activity')->index();
         });
-        
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
+        Schema::dropIfExists('sessions'); // Primero eliminar sesiones por dependencia
+        Schema::dropIfExists('password_reset_tokens'); // Luego eliminar tokens de reinicio de contraseña
+        Schema::dropIfExists('users'); // Finalmente eliminar la tabla de usuarios
     }
 };
